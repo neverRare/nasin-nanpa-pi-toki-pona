89e809cf626799a45f5b707dbcf596bd72bd54e4
neverRare <contactneverrare@gmail.com>
2022-08-04 13:49:24 +0800

    partially implement lexer

--- src/lexer.js
+++ src/lexer.js
@@ -31,6 +31,32 @@ const KEYWORDS = [
   "wan",
   "weka",
 ];
-export function lex(code) {
-  throw new Error("todo");
+export function* lex(code) {
+  let text = "";
+  function* tokenize() {
+    if (text !== "") {
+      let token;
+      if (text[0] === text[0].toUpperCase()) {
+        token = { type: "identifier", identifier: text };
+      } else if (KEYWORDS.includes(text)) {
+        token = { type: "keyword", text };
+      } else {
+        token = { type: "error", message: `mi sona ala e nimi "${text}"` };
+      }
+      text = "";
+      yield token;
+    }
+  }
+  for (const char in code) {
+    if (char.trim() === char) {
+      yield* tokenize();
+    } else if (char === ".") {
+      yield* tokenize();
+      yield { type: "period" };
+    } else if (char === ":") {
+      yield { type: "colon" };
+    } else {
+        text += char;
+    }
+  }
 }
--- src/main.js
+++ src/main.js
@@ -3,4 +3,4 @@ import { lex } from "./lexer.js";
 const decoder = new TextDecoder("utf-8");
 const code = decoder.decode(await Deno.readFile(Deno.args[0]));
 
-const tokens = lex(code);
+const tokens = [...lex(code)];
